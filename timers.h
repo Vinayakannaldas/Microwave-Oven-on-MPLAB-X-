e7b4ce484b261b98b316f7d5b86cd8779206b259
Vinayakannaldas <109888552+Vinayakannaldas@users.noreply.github.com>
2025-07-30 00:17:27 +0530

    Add files via upload

---
+++ timers.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   timers.h
+ * Author: VINAYAK
+ *
+ * Created on 08 May, 2025, 12:26 AM
+ */
+
+
+#ifndef TIMERS_H
+#define	TIMERS_H
+
+
+void init_timer2(void);
+
+#endif	/* TIMERS_H */
+
